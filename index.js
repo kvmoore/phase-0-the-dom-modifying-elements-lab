38389ebc07abd0033891deec38bdf4efcf49dda0
Kenneth <kvmoore097@gmail.com>
2023-09-20 13:51:51 -0500

    phase-1 dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+
+
+main.remove();
+
+
 // Write your code here!
+const element = document.createElement("header");
+const newHeader = document.createElement("h1");
+
+/*has a 'newHeader' variable that points to an <h1> node:
+ReferenceError: newHeader is not defined*/
+
+
+newHeader.id = "victory";
+
+
+
+//after index.js is processed the 'newHeader' variable that points to the <h1> node has an id of 'victory':
+
+
+
+//const newHeader = document.getElementById("newHeader");
+newHeader.innerHTML ="Ken is the champion"
+
+
+/*after index.js is processed the 'newHeader' variable that points to the <h1> node with an id of 'victory' has 
+the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it:*/
